df832a75764f63a31a6496371b7e2eb2dedbbd41
nazzrrg <nazarov.egor2010@yandex.ru>
2020-02-12 12:56:45 +0300

    work in progress

--- Computer Geometry and Graphics/Lab 1/main.cpp
+++ Computer Geometry and Graphics/Lab 1/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+class PNMImage {
+private:
+    char* buffer;
+public:
+    PNMImage (const char* path) {
+        std::error_code ec{};
+        auto size = std::filesystem::file_size(path, ec);
+        if (ec == std::error_code{})
+            std::cout << "size: " << size << '\n';
+        else
+            std::cout << "error when accessing test file, size is: "
+                      << size << " message: " << ec.message() << '\n';
+    }
+
+};
+
+int main(int argc, char** argv) {
+
+    PNMImage image(argv[1]);
+
     return 0;
 }
